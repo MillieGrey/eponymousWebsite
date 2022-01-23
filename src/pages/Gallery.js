511e21c1fd017b16b8f87d96e6ecaf698d0113ad
MillieGrey <garethgreyscale@gmail.com>
2022-01-22 20:32:49 -0600

    okay we gettin there, lightroom iframe is working

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -5,12 +5,23 @@ import photoList from "../assets/photoList.json";
 import React from "react";
 
 const galleryContainer = {
-  paddingTop: "100px",
-  width: "100%",
+  // paddingTop: "100px",
+  display: "flex",
+  // position: 'absolute',
+  height: "100vh",
+  width: "100vw",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const adobeIframe = {
+  width: "90%",
   height: "100%",
-  position: "absolute",
+  // position: "absolute",
+  paddingTop: "100px",
   top: "0",
   left: "0",
+  zIndex: "100",
 };
 
 function Gallery() {
@@ -18,9 +29,10 @@ function Gallery() {
     <Container style={galleryContainer}>
       <iframe
         id='iframe'
-        src='https://lightroom.adobe.com/embed/shares/3a376e5a1a02434ab3b04288c220c04c/slideshow?background_color=%232D2D2D&color=%23999999'
+        src='https://lightroom.adobe.com/embed/shares/3a376e5a1a02434ab3b04288c220c04c/slideshow?&color=%23999999'
         frameborder='0'
         title='gallery'
+        style={adobeIframe}
       ></iframe>
     </Container>
   );
